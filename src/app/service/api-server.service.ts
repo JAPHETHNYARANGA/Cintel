a6e8582d33884707447b3a0c9c9e8b93d4f962f3
JAPHETHNYARANGA <nyaranga4@gmail.com>
2025-02-13 23:20:48 +0300

    created http service class

--- src/app/service/api-server.service.ts
+++ src/app/service/api-server.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiServerService {
 
-  constructor() { }
+  private apiUrl = 'https://jsonplaceholder.typicode.com/users'
+
+  constructor(private http: HttpClient) { }
+
+  //fetch users
+  getUsers(): Observable<any[]>{
+    return this.http.get<any[]>(this.apiUrl)
+  }
 }
