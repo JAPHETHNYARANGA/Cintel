798ef7a7abfaeb91e2ada57f72d743d980250f7b
JAPHETHNYARANGA <nyaranga4@gmail.com>
2025-02-13 23:30:29 +0300

    created initial app structure with complete api fetching

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { debounceTime, catchError } from 'rxjs/operators';
+import { ApiServerService } from './service/api-server.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Cintel';
+export class AppComponent implements OnInit{
+  users: any[] = [];
+  filteredUsers: any[] = [];
+  isLoading: boolean = false;
+  errorMessage: string = '';
+  searchQuery: string = '';
+
+  constructor(private apiServerService: ApiServerService) { }
+
+  ngOnInit(): void {
+    this.isLoading = true;
+    this.apiServerService.getUsers().pipe(
+      debounceTime(300), 
+      catchError((error) => {
+        this.errorMessage = 'An error occurred while fetching data';
+        this.isLoading = false;
+        throw error;
+      })
+    ).subscribe((data: any[]) => {
+      this.users = data;
+      this.filteredUsers = data;
+      this.isLoading = false;
+    });
+  }
+
+  onSearchChange(query: string): void {
+    this.searchQuery = query;
+    this.filteredUsers = this.users.filter(user =>
+      user.name.toLowerCase().includes(query.toLowerCase())
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatInputModule } from '@angular/material/input';  
+import { MatListModule } from '@angular/material/list';    
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
@@ -12,7 +18,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    MatInputModule,
+    MatListModule,   
+    MatProgressSpinnerModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
